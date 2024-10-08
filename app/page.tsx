c7dc933575a23c8c0acf9ac1a816e0a2edf4ab61
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-08 04:09:21 +0300

    Added intro the the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import ThemeToggle from '@/components/theme-toggle'
-import { Button } from '@/components/ui/button'
+import Intro from '@/components/intro'
 import React from 'react'
 
 const page = () => {
   return (
     <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-ex1 font-bold'>Portfolio</h1>
-        <Button>Click me</Button>
+      <div className='container max-w-3xl'>
+        <Intro/>
+        
       </div>
     </section>
   )
