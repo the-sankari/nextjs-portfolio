2ab402be67bbd46ef277b652a4c53a90d258caad
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-10-08 03:56:02 +0300

    Bug fixed for header and theme

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
-import ThemeToggle from './theme-toggle'
+import { ThemeToggle } from './theme-toggle'
 export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
       <nav className='container flex max-w-3xl items-center justify-between'>
         <div>
           <Link href='/' className='font-serif text-2xl font-bold'>
-            HB
+            KDEV
           </Link>
         </div>
 
